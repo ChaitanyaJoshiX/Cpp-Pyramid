fc49d897fe1836eda43c9654334fcc0eb5b8d32c
Chaitanya Joshi <75534893+ChaitanyaJoshiX@users.noreply.github.com>
2021-09-29 21:07:09 +0530

    Add files via upload

---
+++ Pyramid3.cpp
@@ -0,0 +1,39 @@
+/*
+
+Display :
+###########
+##########
+#########
+########
+#######
+######
+#####
+####
+###
+##
+#
+
+*Use nested for only.
+
+Author : @ChaitanyaJoshiX
+
+*/
+
+#include <iostream>
+using namespace std;
+main()
+{
+  int i,j;
+   for(i=10;i>=1;i--)
+   {
+       for(j=1;j<=i;j++)
+       {
+           cout << "#";
+       }
+       cout << endl;
+   }
+}
+
+/*
+Author : @ChaitanyaJoshiX
+*/
